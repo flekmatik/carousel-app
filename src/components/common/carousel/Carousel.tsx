563ea12c514af328251f44279a483b6f5d182c7e
Richard Karmazín <richard@karmazin.cz>
2021-11-18 08:34:23 +0100

    docs

--- src/components/common/carousel/Carousel.tsx
+++ src/components/common/carousel/Carousel.tsx
@@ -80,6 +80,7 @@ class CarouselPure extends React.PureComponent<ICarouselProps, ICarouselState> {
     getItemWidth = () => Math.min(this.props.itemWidth || 200, this.state.width! - 2 * horizontalMargin || Infinity);
 
     handleItemChange = (itemIndex: number, skipOnUpdate?: boolean) => {
+        // Keep focus on component in case focused navigation button is hidden (because first/last item is selected)
         !skipOnUpdate && this.divRef.current?.focus();
         const itemWidth = this.getItemWidth() + 2 * horizontalMargin;
         const left = (itemIndex + 1) * itemWidth;
